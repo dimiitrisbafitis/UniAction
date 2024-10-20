6ee5bd0a400e40ab69d89df821c2d0684c49ef5a
Dimitris Bafitis <bafitisdim@gmail.com>
2024-10-20 11:11:06 +0300

    date

---
+++ datescript.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Λήψη της σημερινής ημερομηνίας σε μορφή YYYY-MM-DD
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); // Οι μήνες ξεκινούν από το 0
+    var yyyy = today.getFullYear();
+
+    today = yyyy + '-' + mm + '-' + dd;
+
+    // Ορισμός του min attribute στο στοιχείο input
+    document.getElementById('Ημερομηνία').setAttribute('min', today);
+});
